b42967401fc8c6e1bc2047f9f76ec07343dbeec1
gulievh <133109621+gulievh@users.noreply.github.com>
2023-05-30 00:43:03 +0300

    Add files via upload

---
+++ UniqueNumbers/UniqueNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace UniqueNumbers
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = { 4, 5, 2, 6, 4, 3 };
+            int[] numbers2 = { 7, 4, 2, 76, 45, 65 };
+
+            Console.WriteLine("output:");
+            foreach (int number1 in numbers1)
+            {
+                bool found = false;
+                foreach (int number2 in numbers2)
+                {
+                    if (number1 == number2)
+                    {
+                        found = true;
+                        break;
+                    }
+                } if (!found)
+                {
+                    Console.WriteLine(number1);
+                }
+            } 
+        }
+    }
+
+}
