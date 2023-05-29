ad6e3959ab3e52cd3428e77a501dc995298b1326
gulievh <133109621+gulievh@users.noreply.github.com>
2023-05-30 00:44:17 +0300

    Add files via upload

---
+++ DoubleNumbers1/DoubleNumbers1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DoubleNumbers1
+{
+    class Program
+    {
+        public static int[] DoubleNumbers1(List<int> numbers)
+    
+        {
+            int[] doubles = new int[numbers.Count];
+            
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                doubles[i] = numbers[i] * 2;
+            }
+            return doubles;
+        }
+        public static void Main(string[] args)
+        {
+           List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            int[] doubles = DoubleNumbers1(numbers);
+
+            foreach (int num in doubles) 
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
